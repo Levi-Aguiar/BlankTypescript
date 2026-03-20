eae06ec7e17f2217fa363fbc42bfa7209b56b0cc
LEVI JOSE DE MELO AGUIAR <levi.jmaguiar@senacsp.edu.br>
2026-03-19 21:40:29 -0300

    ok

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+//import { colors } from "@/style/colors"
+const { colors } = require ("./src/styles/colors"); //Use require em vez de import
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
+  content: [
+    "./src/app/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+    "./App.tsx", "./components/**/*.{ts,tsx}"
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors,
+    },
+  },
+  plugins: [],
+}
